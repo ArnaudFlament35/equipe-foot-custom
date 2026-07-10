52ef0580beaf0138ac82748ce0b2e355b8964506
ArnaudFlament35 <arnaudflam@gmail.com>
2026-07-10 12:19:06 +0200

    first commit

---
+++ equipe-foot-custom.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Equipe Foot Custom
+* Plugin URI:  https://github.com/ArnaudFlament35
+* Description: Ajoutez un CPT pour les joueurs de l'équipe de foot.
+* Version:     1.0.0
+* Author:      Arnaud Flament
+* Author URI:  https://github.com/ArnaudFlament35
+* Text Domain: equipe-foot-custom
+*/
